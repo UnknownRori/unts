dcf611b68e8aecd281815bfbb179545c55b455d2
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-05-28 13:29:22 +0700

    📦 Implement default config

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,4 +1,28 @@
+use std::env;
+
+use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct Config {}
+pub struct Config {
+    database_url: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        dotenv().ok();
+        Self {
+            database_url: env::var("DATABASE_URL").unwrap(),
+        }
+    }
+}
+
+impl<'a> Config {
+    pub fn new() -> Config {
+        todo!();
+    }
+
+    pub fn get_database_url(&'a self) -> &'a str {
+        &self.database_url
+    }
+}
