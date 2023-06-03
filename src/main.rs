5505d87b9f3abb70337efd98b252547a94a31736
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-04 06:18:06 +0700

    🚀 Put the essential to the main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use clap::Parser;
+use unts::app::Application;
+use unts::args::Args;
+
 #[tokio::main]
 async fn main() -> Result<(), &'static str> {
+    let args = Args::parse();
+
+    let _ = Application::new(args).await;
+
     Ok(())
 }
