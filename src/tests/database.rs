c575b03e76b2ae0bda7b4282102d24d5da80d53a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-03 19:55:06 +0700

    📦 Move the test file into seperate directory

--- src/tests/database.rs
+++ src/tests/database.rs
@@ -1,10 +1,9 @@
 #[cfg(test)]
-#[allow(unused_variables)]
 #[tokio::test]
 async fn test_db_create_instance() {
     use crate::{config::Config, database::Database};
 
     let config = Config::default();
 
-    let db = Database::new(&config).await.unwrap();
+    let _ = Database::new(&config).await.unwrap();
 }
