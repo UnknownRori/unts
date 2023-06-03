0308c45d10584cd3813cd2fed6dc0f079b3d0c62
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-04 06:09:49 +0700

    📦 added args and database for `Application`

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,15 +1,17 @@
 use directories::ProjectDirs;
 
-use crate::config::Config;
+use crate::{config::Config, args::Args, database::Database};
 
 #[derive(Debug)]
 pub struct Application {
     config: Config,
+    args: Args,
+    database: Database,
     project_dir: ProjectDirs,
 }
 
 impl Application {
-    pub async fn new() -> Application {
+    pub async fn new(_args: Args) -> Application {
         todo!();
     }
 
