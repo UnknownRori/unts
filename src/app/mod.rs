e87ef7188ffe64acc6f3ec40a1ef65836437cebc
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-21 03:56:03 +0700

    📦 Implement `Application::new()`

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,18 +1,33 @@
 use directories::ProjectDirs;
 
-use crate::{args::Args, config::Config, database::Database};
+use crate::{args::Args, config::Config, database::Database, utility::project_dir};
 
 #[derive(Debug)]
 pub struct Application {
     config: Config,
     args: Args,
-    database: Database,
+    database: Option<Database>,
     project_dir: ProjectDirs,
 }
 
 impl Application {
-    pub async fn new(_args: Args) -> Application {
-        todo!();
+    /// Create new instance of [`Application`] it will panic if cannot find
+    /// directory to store it's data and failed to connect to database
+    pub async fn new(args: Args) -> Application {
+        let config = Config::new(&args);
+        let project_dir = project_dir().expect("(!) Cannot find suitable directory to store");
+        let mut database = None;
+        
+        if !args.offline {
+            database = Some(Database::new(&config).await.expect("(!) Failed to connect database"));
+        }
+
+        Application {
+            config,
+            project_dir,
+            database,
+            args,
+        }
     }
 
     pub async fn run() {
